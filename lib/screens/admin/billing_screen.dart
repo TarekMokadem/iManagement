90ddc020a603c99774e0c3e2e9ead1ae3a07af55
Tarek Mokadem <tarek.mokadem@student.junia.com>
2025-11-01 21:35:09 +0100

    Billing: base Worker URL -> https://imanagement.mokadem59200.workers.dev

--- lib/screens/admin/billing_screen.dart
+++ lib/screens/admin/billing_screen.dart
@@ -10,7 +10,7 @@ class BillingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final tenant = context.watch<TenantProvider>();
-    final billing = BillingService(workerBaseUrl: 'https://mokadem59200.workers.dev');
+    final billing = BillingService(workerBaseUrl: 'https://imanagement.mokadem59200.workers.dev');
     const priceId = 'price_1SOhH0BefWQoVTT0WGmNIldg';
 
     return Scaffold(
